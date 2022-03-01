7d86666c613695e9faaed8ba0ca94c1c26111bdb
zhaohaodang <974513334@qq.com>
2022-03-01 17:33:30 +0800

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
+  
   console.log('req',req)
+  console.log('req.headers',req.headers)
   res.statusCode = 200
   res.json({ name: 'John Doe' })
 }
